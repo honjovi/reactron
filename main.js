3f5f7cb1f42291dc6c082cfbdfff993f17388930
honjovi <honjovi.tarou@gmail.com>
2017-10-20 01:42:44 +0900

    Move app logic to renderer.

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 
 const app = require('electron').app;
 const BrowserWindow = require('electron').BrowserWindow;
-const redux = require('redux');
-const ipcMain = require('electron').ipcMain;
 
 
 app.on('window-all-closed', () => {
@@ -20,32 +18,4 @@ app.on('ready', () => {
 	mainWindow.on('closed', () => {
 		mainWindow = null;
 	});
-	
-	const count = (state, action) => {
-		state = state || 0;
-		switch(action.type) {
-		case "COUNT_UP":
-			return state + 1;
-		}
-		return state;
-	}
-	
-	const myStore = redux.createStore((state, action) => {
-		state = state || {};
-		return {count: count(state.count, action)};
-	});
-	
-	const sendRender = () => {
-		mainWindow.webContents.send("render", myStore.getState())
-	};
-
-	ipcMain.on("dispatch-store", (sender, e) => {
-		myStore.dispatch(e);
-		sendRender();
-	});
-
-	mainWindow.webContents.on("dom-ready", sendRender);
 });
-
-
-
